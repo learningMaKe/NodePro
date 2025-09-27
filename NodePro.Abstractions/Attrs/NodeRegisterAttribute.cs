45d8c9a8fe88037ac207339ca85d4e8a470a44d6
s1mao <s1mao@ASUS>
2025-09-27 12:25:30 +0800

    临时提交

---
+++ NodePro.Abstractions/Attrs/NodeRegisterAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NodePro.Abstractions.Attrs
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class NodeRegisterAttribute:Attribute
+    {
+        public string Tag { get;private set; }
+        public NodeRegisterAttribute(string tag)
+        {
+            Tag = tag;
+        }
+
+    }
+}
